a52f72961993711fafadc7fb13b0cedb9d10022d
CRE2525 <cre2525@gmail.com>
2022-02-01 20:25:33 -0500

    Clean up file structure

--- home.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.contact-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    const contactInfo = document.querySelector('.contact-info');
-    if (contactInfo.classList.contains('contact-no-display')) {
-        contactInfo.classList.remove('contact-no-display');
-    }
-});
